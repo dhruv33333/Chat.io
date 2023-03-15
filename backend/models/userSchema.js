29b1aafa3bea987770e6750f11ec3cee65162d17
dhruv <dhruvnaveen.440@gmail.com>
2023-03-16 01:29:33 +0530

    Update: hash the password before saving in db

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -23,5 +24,12 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.Model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
