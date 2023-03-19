4b764bf861d06e55698f50a557949ede4ec6c8bc
dhruv <dhruvnaveen.440@gmail.com>
2023-03-19 18:39:27 +0530

    Fix: redirect on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
 
 import HomeDashBoard from "./modules/HomeDashboard";
 import Login from "./modules/Login";
 
 function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push("/login");
+  });
+
   return (
     <Switch>
       <Route path="/login" component={Login} />
