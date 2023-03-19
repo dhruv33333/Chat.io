ba8ee70186344b365d6e44c4424a727346ca12b3
dhruv <dhruvnaveen.440@gmail.com>
2023-03-19 19:42:25 +0530

    Add: home page navbar UI

---
+++ frontend/src/modules/HomeDashboard/styled.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+export const NavBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  width: 100%;
+  padding: 5px 10px 5px 10px;
+  border: 5px solid #e2e8f0;
+`;
